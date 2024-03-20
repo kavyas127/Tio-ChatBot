1418b92ee730ee6b5ee7b2ec669bcce841679456
kavyas127 <officialkesai315@gmail.com>
2024-03-20 15:56:49 +0530

    worked on js

--- js/home.js
+++ js/home.js
@@ -2,19 +2,20 @@ window.addEventListener('scroll', function() {
     var logo = document.querySelector('.logo');
     var headerElements = document.querySelector('.header-elements');
     var scrollPosition = window.scrollY;
+    var pageHeight2 = window.innerHeight;
     var pageHeight = window.innerHeight * 2;
 
     if (scrollPosition > pageHeight) {
         logo.style.color = 'white';
     } else {
-        if (scrollPosition > 100) {
+        if (scrollPosition > pageHeight2) {
             logo.style.color = '#11235A';
         } else {
             logo.style.color = 'white';
         }
     }
 
-    if (scrollPosition > 100) {
+    if (scrollPosition > pageHeight2) {
         headerElements.style.color = '#11235A';
         headerElements.style.fontWeight = "600";
     } else {
