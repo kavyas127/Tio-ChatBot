1f97abb414cc3bd774606b54129e0f05c7abaefc
kavyas127 <officialkesai315@gmail.com>
2024-03-20 15:50:35 +0530

    working on 3rd page

--- js/home.js
+++ js/home.js
@@ -1,14 +1,19 @@
 window.addEventListener('scroll', function() {
     var logo = document.querySelector('.logo');
+    var headerElements = document.querySelector('.header-elements');
     var scrollPosition = window.scrollY;
+    var pageHeight = window.innerHeight * 2;
 
-    if (scrollPosition > 100) {
-        logo.style.color = '#11235A'; 
-    } else {
+    if (scrollPosition > pageHeight) {
         logo.style.color = 'white';
+    } else {
+        if (scrollPosition > 100) {
+            logo.style.color = '#11235A';
+        } else {
+            logo.style.color = 'white';
+        }
     }
 
-    var headerElements = document.querySelector('.header-elements'); 
     if (scrollPosition > 100) {
         headerElements.style.color = '#11235A';
         headerElements.style.fontWeight = "600";
